d28541de2872882530431bac0a0368fb6e7f0133
Vliedel <bart@dobots.nl>
2018-04-23 16:04:33 +0200

    Added script to plot recorded samples.

---
+++ plot/plotSamples.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+
+# Config
+RTC_CLOCK_FREQ = 32768
+MAX_RTC_COUNTER_VAL = 0x00FFFFFF
+SAMPLE_TIME_US = 200
+
+allSamples = []
+allTimestamps = []
+
+def main():
+	f = open('voltage.json', 'r')
+	data = json.load(f)
+	for entry in data:
+		# print(entry)
+		samples =   entry['samples']
+		timestamp = entry['timestamp']
+		timestampMs = timestamp * 1000.0 / RTC_CLOCK_FREQ
+		timestampsMs = np.array(range(0,len(samples))) * SAMPLE_TIME_US / 1000.0 + timestampMs
+		global allSamples
+		allSamples.extend(samples)
+		global allTimestamps
+		allTimestamps.extend(timestampsMs)
+	plt.plot(allTimestamps, allSamples)
+	plt.show()
+
+main()
