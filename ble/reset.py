a59116fdadfaa9f937c9b228d021b5dae0739c0d
Vliedel <bart@dobots.nl>
2018-05-17 11:06:48 +0200

    Added hci index to reset script.

--- ble/reset.py
+++ ble/reset.py
@@ -2,13 +2,15 @@
 
 from BluenetLib.BLE import BluenetBle
 
+# Settings
 ADMIN_KEY =  "adminKeyForCrown"
 MEMBER_KEY = "memberKeyForHome"
 GUEST_KEY =  "guestKeyForOther"
 ADDRESS =    "01:23:45:67:89:AB"
+HCI_INDEX =  0
 
 # Initialize the Bluetooth Core
-core = BluenetBle()
+core = BluenetBle(hciIndex=HCI_INDEX)
 core.setSettings(ADMIN_KEY, MEMBER_KEY, GUEST_KEY)
 
 # Connect and send command
