21981fb66ae77ef44581fa80bf8e9fa017e8fd13
Vliedel <bart@dobots.nl>
2018-05-17 11:08:00 +0200

    Add some prints to reset script.

--- ble/reset.py
+++ ble/reset.py
@@ -14,9 +14,13 @@ core = BluenetBle(hciIndex=HCI_INDEX)
 core.setSettings(ADMIN_KEY, MEMBER_KEY, GUEST_KEY)
 
 # Connect and send command
+print("Connecting to", ADDRESS)
 core.connect(ADDRESS)
+print("Send reset command")
 core.control.reset()
+print("Disconnect")
 core.control.disconnect()
 
 # Clean up
+print("Done")
 core.shutDown()
