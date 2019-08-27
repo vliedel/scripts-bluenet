42aaa738199c2a54074c8761527845e755db29d2
vliedel <bart@crownstone.rocks>
2019-08-27 16:11:42 +0200

    Added plotting of power from minicom uart log

---
+++ uart-debug/plotPower.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import numpy as np
+import json
+import sys, os
+import re
+import datetime
+
+# Config
+RTC_CLOCK_FREQ = 32768
+MAX_RTC_COUNTER_VAL = 0x00FFFFFF
+SAMPLE_TIME_US = 200
+
+#currentRegex = "\[.*\] current: (-?\d+ ?)+" # re doesn't support repeating captures.
+samplesPattern      = re.compile("\[.*\] (current|voltage): (.*)")
+timePattern         = re.compile("^\[([^\]]+)\]")
+temperaturePattern  = re.compile(".* T=(-?\d+)")
+currentPattern      = re.compile(".* Crms=(-?\d+)")
+voltagePattern      = re.compile(".* Vrms=(-?\d+)")
+powerPattern        = re.compile(".* P=(-?\d+)")
+forceRelayOnPattern = re.compile(".* forceRelayOn")
+
+timeFormat = "%Y-%m-%d %H:%M:%S.%f"
+
+if (len(sys.argv) != 2):
+	print("Usage: " + sys.argv[0] + " <uart-log-file-name>")
+	exit(1)
+
+# Read the log file and store in a string array
+with open(sys.argv[1], 'r') as file:
+	data = file.readlines()
+
+currentSamples = []
+currentSamplesTime = []
+voltageSamples = []
+voltageSamplesTime = []
+temperatures = []
+temperaturesTime = []
+relays = []
+relaysTime = []
+currents = []
+currentsTime = []
+voltages = []
+voltagesTime = []
+powers = []
+powersTime = []
+
+timestamp = 0
+firstTimestamp = 0
+temperature = 0
+relay = 0
+current = 0
+voltage = 0
+power = 0
+for line in data:
+	addedCurrentSamples = False
+	addedVoltageSamples = False
+	match = timePattern.match(line)
+	if (match):
+		time = datetime.datetime.strptime(match.group(1), timeFormat)
+		timestamp = time.timestamp()
+		if (firstTimestamp == 0):
+			firstTimestamp = timestamp
+		timestamp -= firstTimestamp
+	
+	match = samplesPattern.match(line)
+	if (match):
+		samples = []
+		sampleTypeStr = match.group(1)
+		sampleStrings = match.group(2).strip().split(' ')
+		for s in sampleStrings:
+			samples.append(int(s))
+		timestamps = timestamp + np.array(range(0, len(samples))) * SAMPLE_TIME_US / 1000 / 1000
+		if (sampleTypeStr == "current"):
+			currentSamples.append(samples)
+			currentSamplesTime.append(timestamps)
+		else:
+			voltageSamples.append(samples)
+			voltageSamplesTime.append(timestamps)
+	match = temperaturePattern.match(line)
+	if (match):
+		temperature = int(match.group(1).strip())
+		temperatures.append(temperature)
+		temperaturesTime.append(timestamp)
+	match = forceRelayOnPattern.match(line)
+	if (match):
+		relay = 1
+		relays.append(relay)
+		relaysTime.append(timestamp)
+		relays.append(0)
+		relaysTime.append(timestamp)
+	match = currentPattern.match(line)
+	if (match):
+		current = int(match.group(1).strip())
+		currents.append(current)
+		currentsTime.append(timestamp)
+	match = voltagePattern.match(line)
+	if (match):
+		voltage = int(match.group(1).strip())
+		voltages.append(voltage)
+		voltagesTime.append(timestamp)
+	match = powerPattern.match(line)
+	if (match):
+		power = int(match.group(1).strip())
+		powers.append(power)
+		powersTime.append(timestamp)
+
+fig = plt.figure()
+gs = GridSpec(9, 1)
+axs = []
+axs.append(fig.add_subplot(gs[0:3, 0]))
+for i in range(1, 7):
+	axs.append(fig.add_subplot(gs[i+2, 0], sharex=axs[0]))
+for i in range(0, 6):
+	axs[i].xaxis.set_visible(False)
+
+#fig, axs = plt.subplots(7, 1, sharex=True)
+
+axs[0].plot(np.array(currentSamplesTime).transpose(), np.array(currentSamples).transpose(), label="current")
+axs[1].plot(np.array(voltageSamplesTime).transpose(), np.array(voltageSamples).transpose(), label="voltage")
+axs[2].plot(temperaturesTime, temperatures, label="temperature")
+axs[3].plot(relaysTime, relays, label="forceRelayOn")
+axs[4].plot(currentsTime, currents, label="Irms")
+axs[5].plot(voltagesTime, voltages, label="Vrms")
+axs[6].plot(powersTime, powers, label="P")
+
+for i in range(2, 7):
+	axs[i].legend()
+
+#plt.figure()
+#plt.plot(np.array(currentSamplesTime).transpose(), np.array(currentSamples).transpose())
+plt.show()
