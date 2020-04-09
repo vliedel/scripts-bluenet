0b13c2b7c9c86f64b152385529e9c49e77bee267
vliedel <bart@crownstone.rocks>
2020-04-09 13:55:55 +0200

    Calculate power like in fw.

--- uart-debug/plotPower.py
+++ uart-debug/plotPower.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Parses a uart logfile and plots the results.
+# Make sure the regex patterns and time format are correct.
+
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import numpy as np
@@ -12,14 +15,18 @@ import datetime
 RTC_CLOCK_FREQ = 32768
 MAX_RTC_COUNTER_VAL = 0x00FFFFFF
 SAMPLE_TIME_US = 200
+NUM_SAMPLES = 100
+VOLTAGE_MULTIPLIER = -0.253
+CURRENT_MULTIPLIER = 0.0071
 
 #currentRegex = "\[.*\] current: (-?\d+ ?)+" # re doesn't support repeating captures.
-samplesPattern      = re.compile("\[.*\] (current|voltage): (.*)")
+samplesPattern      = re.compile("\[.*\] (current|voltage): ([0-9\- ]*)")
 timePattern         = re.compile("^\[([^\]]+)\]")
 temperaturePattern  = re.compile(".* T=(-?\d+)")
 currentPattern      = re.compile(".* Crms=(-?\d+)")
 voltagePattern      = re.compile(".* Vrms=(-?\d+)")
 powerPattern        = re.compile(".* P=(-?\d+)")
+currentZeroPattern  = re.compile(".* C0=(-?\d+)")
 forceRelayOnPattern = re.compile(".* forceRelayOn")
 
 timeFormat = "%Y-%m-%d %H:%M:%S.%f"
@@ -32,6 +39,7 @@ if (len(sys.argv) != 2):
 with open(sys.argv[1], 'r') as file:
 	data = file.readlines()
 
+# Start parsing
 currentSamples = []
 currentSamplesTime = []
 voltageSamples = []
@@ -46,6 +54,16 @@ voltages = []
 voltagesTime = []
 powers = []
 powersTime = []
+currentZeros = []
+currentZerosTime = []
+
+# Calculated values
+powerCalcs = []
+powerCalcsTime = []
+currentMeans = []
+currentMeansTime = []
+voltageMeans = []
+voltageMeansTime = []
 
 timestamp = 0
 firstTimestamp = 0
@@ -54,6 +72,11 @@ relay = 0
 current = 0
 voltage = 0
 power = 0
+currentZero = 0
+voltageZero = 0
+powerCalc = 0.0
+currentMean = 0.0
+voltageMean = 0.0
 for line in data:
 	addedCurrentSamples = False
 	addedVoltageSamples = False
@@ -70,15 +93,44 @@ for line in data:
 		samples = []
 		sampleTypeStr = match.group(1)
 		sampleStrings = match.group(2).strip().split(' ')
-		for s in sampleStrings:
-			samples.append(int(s))
-		timestamps = timestamp + np.array(range(0, len(samples))) * SAMPLE_TIME_US / 1000 / 1000
-		if (sampleTypeStr == "current"):
-			currentSamples.append(samples)
-			currentSamplesTime.append(timestamps)
-		else:
-			voltageSamples.append(samples)
-			voltageSamplesTime.append(timestamps)
+		mean = 0.0
+		if (len(sampleStrings) == NUM_SAMPLES):
+			for s in sampleStrings:
+				try:
+					sample = int(s)
+				except:
+					print("Invalid sample value:", s, "in line:", line)
+					exit(1)
+				samples.append(sample)
+				mean += sample
+			mean = mean / len(samples)
+			timestamps = timestamp + np.array(range(0, len(samples))) * SAMPLE_TIME_US / 1000 / 1000
+			if (sampleTypeStr == "current"):
+				currentSamples.append(samples)
+				currentSamplesTime.append(timestamps)
+				currentMean = mean
+				currentMeans.append(currentMean)
+				currentMeansTime.append(timestamp)
+			else:
+				voltageSamples.append(samples)
+				voltageSamplesTime.append(timestamps)
+				voltageMean = mean
+				voltageMeans.append(voltageMean)
+				voltageMeansTime.append(timestamp)
+				
+				# Calculate power
+				if (len(currentSamples) > 0):
+					voltageZero = 0.0
+					for i in range(0, NUM_SAMPLES):
+						voltageZero += voltageSamples[-1][i]
+					voltageZero = voltageZero / NUM_SAMPLES
+					powerCalc = 0
+					for i in range(0, NUM_SAMPLES):
+						powerCalc = powerCalc + (voltageSamples[-1][i] - voltageZero) * (currentSamples[-1][i] - currentZero)
+					powerCalc = powerCalc * VOLTAGE_MULTIPLIER * CURRENT_MULTIPLIER * 1000 / NUM_SAMPLES
+					powerCalcs.append(powerCalc)
+					powerCalcsTime.append(timestamp)
+	
 	match = temperaturePattern.match(line)
 	if (match):
 		temperature = int(match.group(1).strip())
@@ -106,7 +158,13 @@ for line in data:
 		power = int(match.group(1).strip())
 		powers.append(power)
 		powersTime.append(timestamp)
+	match = currentZeroPattern.match(line)
+	if (match):
+		currentZero = int(match.group(1).strip()) / 1000.0
+		currentZeros.append(currentZero)
+		currentZerosTime.append(timestamp)
 
+# Plot the results
 fig = plt.figure()
 gs = GridSpec(9, 1)
 axs = []
@@ -117,14 +175,17 @@ for i in range(0, 6):
 	axs[i].xaxis.set_visible(False)
 
 #fig, axs = plt.subplots(7, 1, sharex=True)
-
-axs[0].plot(np.array(currentSamplesTime).transpose(), np.array(currentSamples).transpose(), label="current")
-axs[1].plot(np.array(voltageSamplesTime).transpose(), np.array(voltageSamples).transpose(), label="voltage")
-axs[2].plot(temperaturesTime, temperatures, label="temperature")
-axs[3].plot(relaysTime, relays, label="forceRelayOn")
-axs[4].plot(currentsTime, currents, label="Irms")
-axs[5].plot(voltagesTime, voltages, label="Vrms")
-axs[6].plot(powersTime, powers, label="P")
+axs[0].plot(currentMeansTime, currentMeans, label="Cmean")
+axs[1].plot(voltageMeansTime, voltageMeans, label="Vmean")
+#axs[0].plot(np.array(currentSamplesTime).transpose(), np.array(currentSamples).transpose(), label="current")
+#axs[0].plot(currentZerosTime, currentZeros)
+#axs[1].plot(np.array(voltageSamplesTime).transpose(), np.array(voltageSamples).transpose(), label="voltage")
+#axs[2].plot(temperaturesTime, temperatures, label="temperature")
+#axs[3].plot(relaysTime, relays, label="forceRelayOn")
+#axs[4].plot(currentsTime, currents, label="Irms")
+#axs[5].plot(voltagesTime, voltages, label="Vrms")
+#axs[6].plot(powersTime, powers, label="P")
+#axs[6].plot(powerCalcsTime, powerCalcs, label="Pcalc")
 
 for i in range(2, 7):
 	axs[i].legend()
