d6b91dbc33790d10ae272925784a7188bd43c6fc
vliedel <bart@crownstone.rocks>
2020-08-25 15:28:10 +0200

    Fit the data to calculate the multipliers.

--- calibrate-power/calibrate.py
+++ calibrate-power/calibrate.py
@@ -4,6 +4,7 @@ import json
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import *
 
 PLOT_SAMPLES = False
 
@@ -78,7 +79,7 @@ for fileName in fileNames:
 	# Plot std
 	ax1.plot([voltageGroundTruth, voltageGroundTruth], [voltageRms[0] - voltageRms[1], voltageRms[0] + voltageRms[1]], '-k')
 	ax2.plot([currentGroundTruth, currentGroundTruth], [currentRms[0] - currentRms[1], currentRms[0] + currentRms[1]], '-k')
-	ax3.plot([powerGroundTruth, powerGroundTruth], [powerRms[0] - powerRms[1], powerRms[0] + powerRms[1]], '-k')
+	# ax3.plot([powerGroundTruth, powerGroundTruth], [powerRms[0] - powerRms[1], powerRms[0] + powerRms[1]], '-k')
 
 	if PLOT_SAMPLES:
 		for i in range(0, len(voltageSamples)):
@@ -87,8 +88,48 @@ for fileName in fileNames:
 			ax22.plot(x, currentSamples[i], '.-')
 			t += len(voltageSamples[i])
 
+# Fit the data: calibrated = x[1] + x[0] * truth
+voltageFit = np.polyfit(voltageTruth, voltageMean, 1)
+currentFit = np.polyfit(currentTruth, currentMean, 1)
+
+# Fit the data: calibrated = 0 + x[0] * truth
+optimize_func = lambda x: x[0] * np.array(voltageTruth) - np.array(voltageMean)
+beta0 = [0]
+lsq = least_squares(optimize_func, beta0, method='lm', loss='linear')
+voltageFit = np.array([lsq.x[0], 0])
+
+optimize_func = lambda x: x[0] * np.array(currentTruth) - np.array(currentMean)
+beta0 = [0]
+lsq = least_squares(optimize_func, beta0, method='lm', loss='linear')
+currentFit = np.array([lsq.x[0], 0])
+
+voltageMultiplier = 1.0 / voltageFit[0]
+currentMultiplier = 1.0 / currentFit[0]
+print("voltageMultiplier:", voltageMultiplier)
+print("currentMultiplier:", currentMultiplier)
+
+
+# Use the multipliers to scale the power
+powerMean = np.array(powerMean) * voltageMultiplier * currentMultiplier
+
+
 # Plot all truth vs mean
-ax1.plot(voltageTruth, voltageMean, 'o')
-ax2.plot(currentTruth, currentMean, 'o')
-ax3.plot(powerTruth, powerMean, 'o')
+ax1.plot(voltageTruth, voltageMean, 'o', label="calculated")
+ax1.plot(voltageTruth, np.poly1d(voltageFit)(voltageTruth), label="fit")
+ax1.set_xlabel("truth (V)")
+ax1.set_ylabel("calculated (V)")
+ax1.set_title('Voltage multiplier=' + str(voltageMultiplier))
+
+ax2.plot(currentTruth, currentMean, 'o', label="calculated")
+ax2.plot(currentTruth, np.poly1d(currentFit)(currentTruth), label="fit")
+ax2.set_xlabel("truth (A)")
+ax2.set_ylabel("calculated (A)")
+ax2.set_title('Current multiplier=' + str(currentMultiplier))
+
+ax3.plot(powerTruth, powerMean, 'o', label="power")
+ax3.set_xlabel("truth (W)")
+ax3.set_ylabel("calculated (W)")
+ax3.set_title('Power')
+
+fig.tight_layout()
 plt.show()
