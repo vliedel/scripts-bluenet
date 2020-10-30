69220f4834fe1fe2a995c867c2655acf328273a1
vliedel <bart@crownstone.rocks>
2020-10-30 17:53:53 +0100

    print rms

--- plot/plotSamples2.py
+++ plot/plotSamples2.py
@@ -6,6 +6,9 @@ import sys, os
 from enum import Enum
 import re
 
+
+# from ..record import parse_recorded_voltage
+# from ..record import parse_app_files
 sys.path.append('../record')
 import parse_recorded_voltage
 import parse_app_files
@@ -23,6 +26,27 @@ FilteredPattern = re.compile(".*power-samples-filteredData.*")
 UnfilteredPattern = re.compile(".*power-samples-unfilteredData.*")
 SoftFusePattern = re.compile(".*power-samples-softFuseData.*")
 
+currentMultiplier = 0.0045
+voltageMultiplier = 0.2
+
+def calcCurrentOrVoltageRms(samples, multiplier):
+	zero = calcZero(samples)
+	rms = calcRms(samples - zero, samples - zero)
+	rms *= multiplier
+	return rms
+
+def calcRms(samples1, samples2, square=True):
+	rms = 0.0
+	for i in range(0, len(samples1)):
+		rms += samples1[i] * samples2[i]
+	rms /= len(samples1)
+	if (square):
+		return np.sqrt(rms)
+	else:
+		return rms
+
+def calcZero(samples):
+	return np.mean(samples)
 
 def main():
 	fileNames = sys.argv[1:]
@@ -45,11 +69,18 @@ def main():
 				or (samplesType == PowerSampleType.NonTriggeredSwitchcraft) \
 				or (samplesType == PowerSampleType.SoftFuse):
 			voltageAndCurrent = False
+		print("samplesType:", samplesType.name)
 
 		if voltageAndCurrent:
 			fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+			ax1.set_ylabel("Voltage (ADC value)")
+			ax1.set_xlabel("Time (ms)")
+			ax2.set_ylabel("Current (ADC value)")
+			ax2.set_xlabel("Time (ms)")
 		else:
 			fig, ax1 = plt.subplots(1, sharex=True)
+			ax1.set_ylabel("ADC value")
+			ax1.set_xlabel("Time (ms)")
 		fig.suptitle(fileName)
 
 		# Parse file
@@ -61,11 +92,19 @@ def main():
 		if not voltageAndCurrent:
 			for i in range(0, len(allTimestamps)):
 				ax1.plot(allTimestamps[i], allSamples[i], '.-')
+				multiplier = voltageMultiplier
+				if (samplesType == PowerSampleType.SoftFuse):
+					multiplier = currentMultiplier
+				rms = calcCurrentOrVoltageRms(allSamples[i], multiplier)
+				print("i=", i, " rms=", rms)
 
 		else:
 			for i in range(0, len(allTimestamps)):
 				ax1.plot(allTimestamps[i][0:100], allSamples[i][0:100], '.-')
 				ax2.plot(allTimestamps[i][0:100], allSamples[i][100:200], '.-')
+				voltageRms = calcCurrentOrVoltageRms(allSamples[i][0:100], voltageMultiplier)
+				currentRms = calcCurrentOrVoltageRms(allSamples[i][100:200], currentMultiplier)
+				print("i=", i, " Vrms=", voltageRms, " Irms=", currentRms)
 
 	plt.show()
 
