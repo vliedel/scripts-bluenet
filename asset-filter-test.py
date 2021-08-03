1bf0e0ba550ae1eea35e14a1e1cb8e7ea1c8aa6f
vliedel <bart@crownstone.rocks>
2021-08-03 17:38:34 +0200

    Updated asset filter test.

--- asset-filter-test.py
+++ asset-filter-test.py
@@ -3,17 +3,13 @@
 """
 Client to show binary logs.
 """
-import time
 import argparse
 import os
 import asyncio
 
-from crownstone_core.packets.assetFilter.FilterDescriptionPackets import *
-from crownstone_core.packets.assetFilter.FilterIOPackets import *
-from crownstone_core.packets.assetFilter.FilterMetaDataPackets import *
-from crownstone_core.util import AssetFilterUtil
-from crownstone_core.util.AssetFilterUtil import get_filter_crc
-from crownstone_core.util.Cuckoofilter import CuckooFilter
+from crownstone_core.packets.assetFilter.FilterMetaDataPackets import FilterType
+from crownstone_core.packets.assetFilter.builders.AssetFilter import AssetFilter
+from crownstone_core.packets.assetFilter.util import AssetFilterMasterCrc
 from crownstone_uart import CrownstoneUart
 
 from bluenet_logs import BluenetLogs
@@ -41,6 +37,8 @@ argParser.add_argument('--device',
                        help='The UART device to use, for example: /dev/ttyACM0')
 args = argParser.parse_args()
 
+
+
 sourceFilesDir = args.sourceFilesDir
 
 
@@ -58,91 +56,67 @@ async def main():
 	try:
 		print(f"Listening for logs and using files in \"{sourceFilesDir}\" to find the log formats.")
 		await uart.initialize_usb(port=args.device, writeChunkMaxSize=64)
-#		logging.DEBUG("init done")
-
-		# await asyncio.sleep(1)
 
 		filters = await uart.control.getFilterSummaries()
 		print(filters)
-		masterVersion = filters.masterVersion.val
+		masterVersion = filters.masterVersion
 
 		##############################################################################################
 		##################################### Remove all filters #####################################
 		##############################################################################################
 
 		print("Remove all filters")
-		for f in filters.summaries.val:
-			await uart.control.removeFilter(f.filterId)
+		for f in filters.summaries:
+			print(f"    Remove id={f.id}")
+			await uart.control.removeFilter(f.id)
 
-		masterVersion += 1
-		filtersAndIds = []
-		masterCrc = AssetFilterUtil.get_master_crc_from_filters(filtersAndIds)
-		print("Master CRC:", masterCrc)
-		print("Commit")
-		await uart.control.commitFilterChanges(masterVersion, masterCrc)
+		# masterVersion += 1
+		# filtersAndIds = []
+		# masterCrc = AssetFilterUtil.get_master_crc_from_filters(filtersAndIds)
+		# print("Master CRC:", masterCrc)
+		# print("Commit")
+		# await uart.control.commitFilterChanges(masterVersion, masterCrc)
 
 		#############################################################################################
 		##################################### Create new filter #####################################
 		#############################################################################################
 
-		filterInput = FilterInputDescription()
-		filterInput.format.type = AdvertisementSubdataType.MAC_ADDRESS
-
-		filterOutputDescription = FilterOutputDescription()
-		filterOutputDescription.out_format.type = FilterOutputFormat.MAC_ADDRESS
-		# filterOutputDescription.in_format = None
-
-		metadata = FilterMetaData()
-		metadata.type = FilterType.CUCKOO
-		metadata.profileId = 255
-		metadata.inputDescription = filterInput
-		metadata.outputDescription = filterOutputDescription
-
-		# Create the cuckoo filter
-		max_buckets_log2 = 3
-		nests_per_bucket = 4
-		load_factor = 0.75
-		cuckooFilter = CuckooFilter(max_buckets_log2, nests_per_bucket)
-		max_items = cuckooFilter.fingerprintcount()
-		num_items_to_test = int(max_items * load_factor)
-		for i in range(num_items_to_test):
-			if not cuckooFilter.add([i]):
-				print("Failed to add to cuckoo filter")
-				raise(Exception)
-		cuckooFilterData = cuckooFilter.getData()
-
-		filter = AssetFilter()
-		filter.metadata = metadata
-		filter.filterdata.val = cuckooFilterData
-
-		print("Filter size:", len(filter.getPacket()))
-		print("Filter CRC:", get_filter_crc(filter))
-
-
-		###########################################################################################
-		##################################### Uploade filters #####################################
-		###########################################################################################
-
-		filters = [filter, filter, filter]
-
 		filterId = 0
+		filter = AssetFilter()
+		filter.setFilterId(filterId)
+		filter.filterByMacAddress(["01:23:45:67:89:AB", "01:23:45:67:89:CD", "01:23:45:67:89:EF"])
+		filter.outputMacRssiReport()
+		filter.setProfileId(0)
+		print(filter)
+		print(filter.toBuffer())
+		print("Filter size:", len(filter.toBuffer()))
+		print("Filter CRC:", filter.getCrc())
+
+		filterId += 1
+		filter2 = AssetFilter()
+		filter2.setFilterId(filterId)
+		filter2.filterByNameWithWildcards("CR?N")
+		filter2.outputAssetId().basedOnName()
+		filter2.setFilterType(FilterType.CUCKOO)
+		print(filter2)
+		print(filter2.toBuffer())
+		print("Filter size:", len(filter2.toBuffer()))
+		print("Filter CRC:", filter2.getCrc())
+
+		filters = [filter, filter2]
+
+
+		##########################################################################################
+		##################################### Upload filters #####################################
+		##########################################################################################
+
 		masterVersion += 1
 
-		filtersAndIds = []
 		for f in filters:
-			filtersAndIds.append(AssetFilterAndId(filterId, filter))
-			filterId += 1
-
-		masterCrc = AssetFilterUtil.get_master_crc_from_filters(filtersAndIds)
-		print("Master CRC:", masterCrc)
-
-		filterId = 0
-		for f in filters:
-			print(f"Upload filter {filterId}")
-			await uart.control.uploadFilter(filterId, f)
-			filterId += 1
+			print(f"Upload filter {f.getFilterId()}")
+			await uart.control.uploadFilter(f)
 		print("Commit")
-		await uart.control.commitFilterChanges(masterVersion, masterCrc)
+		await uart.control.commitFilterChanges(masterVersion, filters)
 
 		# Simply keep the program running.
 		while True:
@@ -150,7 +124,6 @@ async def main():
 	except KeyboardInterrupt:
 		pass
 	finally:
-		# time.sleep(1)
 		print("\nStopping UART..")
 		uart.stop()
 		print("Stopped")
